04796b7e59264a7f98b4517cf7c8b6d6b5627494
Valentoine <vava78@live.fr>
2021-03-19 12:41:31 +0100

    first commit 2

--- app/helpers/test_helper.rb
+++ app/helpers/test_helper.rb
@@ -3,12 +3,27 @@ include PyCall::Import
 
 module TestHelper
   PyCall.init('python3')
+  
+  # Run in console : python3 -m pip install XXX
 
-  pyimport :os
-  p os.uname
+  PyCall.exec('import os')
+  PyCall.exec('import sys')
+  PyCall.exec('import pandas as pd')
+  PyCall.exec('import numpy as np')
 
-  PyCall.exec 'def double(x): return x * 2'
+  PyCall.exec("df = pd.read_csv('../../storage/data_warehouse.csv', sep=',', encoding='latin-1')")
 
-  p PyCall.eval('double(123)')
+  p PyCall.eval("'First information about the csv'")
+
+  p PyCall.eval("df.shape")
+  p PyCall.eval("''")
+  p PyCall.eval("df.describe()")
+  p PyCall.eval("''")
+  p PyCall.eval("df.dtypes")
+  p PyCall.eval("''")
+
+  p PyCall.eval('df.head(10)')
+
+  # p PyCall.eval('double(123)')
 end
 
